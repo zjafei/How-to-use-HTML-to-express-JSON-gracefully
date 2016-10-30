8886603f636744bd11cb715b534fa4db1ada4639
eric ma <924402086@qq.com>
2016-10-30 18:02:10 +0800

    基本上都好了 就是最后每个模块的html如何优雅的进来指定容器是个问题

--- htmlToJson.js
+++ htmlToJson.js
@@ -5,8 +5,16 @@
  * Time: 22:19
  */
 var h2j = {
+    pageJson: {},
+    modelHtml: {},
+    trim: function (str) {
+        return str.replace(/(^\s*)|(\s*$)/g, '');
+    },
     regAttrName: function (str) {//匹配特定属性名称
         return /^data-([a-z]+\-)*[a-z]+$/.test(str);
+    },
+    getModelName: function (str) {
+        return this.toHump(this.trim(str));
     },
     toHump: function (str) {
         var r = /-(\w)/g;
@@ -14,11 +22,50 @@ var h2j = {
             return x.slice(1).toUpperCase();
         });
     },
+    toHyphen: function (str) {
+        return str.replace(/([A-Z])/g, "-$1").toLowerCase();
+    },
     getKeyNameValue: function (str) {
-        var re = /\n/g, lr = /(^\s*)|(\s*$)/g;
-        var h = str.split('<')[0].replace(re, "").replace(lr, "");
-        var e = str.split('>').pop().replace(re, "").replace(lr, "");
+        var re = /\n/g;
+        var h = this.trim(str.split('<')[0].replace(re, ""));
+        var e = this.trim(str.split('>').pop().replace(re, ""));
         return h + e;
+    },
+    // getFunctionName:function(){
+    //     return this.name || this.toString().match(/function\s*([^(]*)\(/)[1]
+    // },
+    createHtml: function (ary) {
+        var myThis = this;
+        $.each(ary, function (i, e) {
+            var f = eval(e);
+            if (typeof f === 'function') {
+                if (typeof myThis.pageJson[e] === 'object') {
+                    myThis.modelHtml[e] = f(myThis.pageJson[e]);
+                } else {
+                    console.log('the module ' + myThis.toHump(e) + ' does not exist!');
+                }
+            } else if ($.isArray(e)) {
+                f = eval(e[0]);
+                if (typeof f === 'function') {
+                    var n = myThis.toHump(e[1]);
+                    if (typeof myThis.pageJson[n] === 'object') {
+                        myThis.modelHtml[n] = f(myThis.pageJson[n]);
+                    } else {
+                        console.log('the module ' + e[1] + ' does not exist!');
+                    }
+                } else {
+                    console.log(e[0] + ' is not a function!');
+                }
+            } else {
+                console.log('type of data must be array or string!');
+            }
+        });
+    },
+    rendering: function (ary, callback) {
+        var cb = callback || function () {
+            };
+        this.createHtml(ary);
+        cb(this.modelHtml);
     },
     getJson: function ($dom) {//获取html转变过来的json
         var o,
@@ -81,5 +128,23 @@ var h2j = {
             }
         }
         return o;
+    },
+    processingModelDom: function () {
+        var myThis = this,
+            $dom = $('[model]');
+        $dom.each(function (i, e) {
+            var $e = $(e);
+            var n = myThis.getModelName($e.attr('model'));
+            if (n) {
+                //var a = myThis.getModelName($e.attr('model'));
+                myThis.pageJson[myThis.getModelName($e.attr('model'))] = myThis.getJson($e);
+                $e.remove();
+                $e = null;
+                // console.log($e.text());
+                //myThis.pageHtmlFrame[a] = $('<div id="' + n + '"></div>');
+            }
+        });
     }
 };
+
+h2j.processingModelDom();
